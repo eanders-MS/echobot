6c5f68a3dabbe67f201892dc8f83f732c0d19859
Eric Anderson <eanders@microsoft.com>
2016-05-15 09:31:40 -0700

    add channeldata command

--- server.js
+++ server.js
@@ -2,17 +2,42 @@ var restify = require('restify');
 var builder = require('botbuilder');
 
 // Get secrets from server environment
-var botConnectorOptions = { 
-    appId: process.env.BOTFRAMEWORK_APPID, 
-    appSecret: process.env.BOTFRAMEWORK_APPSECRET 
+var botConnectorOptions = {
+    appId: process.env.BOTFRAMEWORK_APPID,
+    appSecret: process.env.BOTFRAMEWORK_APPSECRET
 };
 
 // Create bot
 var bot = new builder.BotConnectorBot(botConnectorOptions);
 bot.add('/', function (session) {
-    
+
     //respond with user's message
     session.send("You said " + session.message.text);
+});
+bot.add('/channeldata', function(session) {
+    session.send({
+        type: "Message",
+        to: session.message.from,
+        from: session.message.to,
+        replyToMessageId: session.message.id,
+        conversationId: session.message.conversationId,
+        channelConversationId: session.message.channelConversationId,
+        channelMessageId: session.message.channelMessageId,
+        participants: session.message.participants,
+        totalParticipants: session.message.totalParticipants,
+        channelData:
+        {
+            "method": "sendSticker",
+            "parameters":
+            {
+                "sticker":
+                {
+                    "url": "https://upload.wikimedia.org/wikipedia/commons/3/33/LittleCarron.gif",
+                    "mediaType": "image/gif"
+                }
+            }
+        }
+    });
 });
 
 // Setup Restify Server
@@ -28,5 +53,5 @@ server.get(/.*/, restify.serveStatic({
 }));
 
 server.listen(process.env.port || 3978, function () {
-    console.log('%s listening to %s', server.name, server.url); 
+    console.log('%s listening to %s', server.name, server.url);
 });
